d01e5e1e0e87dded44a4625c927aa558e18439d3
Aleksandr Anosov <you@example.com>
2025-05-22 03:18:40 +0300

    correct guice

--- src/test/java/di/TestModule.java
+++ src/test/java/di/TestModule.java
@@ -9,15 +9,10 @@ import org.openqa.selenium.WebDriver;
 public class TestModule extends AbstractModule {
     @Override
     protected void configure() {
-        // 1) Провайдер браузера — единый Singleton
-        bind(WebDriverProvider.class)
-            .in(Singleton.class);
-
-        // 2) WebDriver в рамках одного Cucumber-сценария
+        bind(WebDriverProvider.class).in(Singleton.class);
         bind(WebDriver.class)
             .toProvider(WebDriverProvider.class)
             .in(ScenarioScoped.class);
-
-        // (Не нужно бинжить страницы/компоненты — они аннотированы @ScenarioScoped)
     }
 }
+
